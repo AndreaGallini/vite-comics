3f49bd2a6996e73aeaa9a8525d5fc85351ab2ee8
AndreaGallini <andrea.gallini01@libero.it>
2022-11-10 13:50:13 +0100

    add img

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>ciao</h1>
+  <HeaderComponent />
+</template>
+
+<script>
+import HeaderComponent from "./components/HeaderComponent.vue";
+
+export default {
+  components: {
+    HeaderComponent,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
