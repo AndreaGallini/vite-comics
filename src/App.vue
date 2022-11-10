2b25fe6a7f495c3b733f26753e7d098c23941dc5
AndreaGallini <andrea.gallini01@libero.it>
2022-11-10 14:23:39 +0100

    footerbottom fatto base

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
-  <HeaderComponent />
+  <div class="container">
+    <HeaderComponent />
+    <FooterTopComponent />
+  </div>
 </template>
 
 <script>
 import HeaderComponent from "./components/HeaderComponent.vue";
+import FooterTopComponent from "./components/FooterTopComponent.vue";
 
-export default { components: { HeaderComponent } };
+export default {
+  components: {
+    HeaderComponent,
+    FooterTopComponent,
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
